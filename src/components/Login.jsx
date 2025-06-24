b3e76b91b0b5694466ee299e9283039416fa7026
Smi-lin <olajideolabimpe26@gmail.com>
2025-06-24 16:56:26 +0100

    update on code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ const LoginSystem = ({ onLoginSuccess }) => {
 
   const validCredentials = {
     username: import.meta.env.VITE_USERNAME,
-    password: import.meta.env.VITE_PWD
+    password: import.meta.env.VITE__PWD
   };
 
   const [username, setUsername] = useState('');
